aa236ddaf5dbbed3fb95f1ecd875bd3c20afb075
Rishat Suleymanov <spk_irbis@mail.ru>
2021-09-27 18:34:58 +0300

    Repository

--- src/main/java/ru/netology/netologydiplom/repository/CustomerFileRepository.java
+++ src/main/java/ru/netology/netologydiplom/repository/CustomerFileRepository.java
@@ -1,10 +1,12 @@
 package ru.netology.netologydiplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.netology.netologydiplom.entity.CustomersFile;
 
 import java.util.List;
 
+@Repository
 public interface CustomerFileRepository extends JpaRepository<CustomersFile, Long> {
 
     CustomersFile findByFileName(String fileName);
--- src/main/java/ru/netology/netologydiplom/repository/CustomerRepository.java
+++ src/main/java/ru/netology/netologydiplom/repository/CustomerRepository.java
@@ -1,7 +1,11 @@
 package ru.netology.netologydiplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.netology.netologydiplom.entity.Customer;
 
+@Repository
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
+    Customer findByLogin(String login);
+
 }
