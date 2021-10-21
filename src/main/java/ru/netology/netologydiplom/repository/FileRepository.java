8f5f582105d9c8151368cfffeda007b34ff0555f
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-21 11:33:52 +0300

    Repository annotation

--- src/main/java/ru/netology/netologydiplom/repository/FileRepository.java
+++ src/main/java/ru/netology/netologydiplom/repository/FileRepository.java
@@ -1,12 +1,14 @@
 package ru.netology.netologydiplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.netology.netologydiplom.entity.File;
 import ru.netology.netologydiplom.entity.User;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
 public interface FileRepository extends JpaRepository<File, Long> {
 
     List<File> findAllByUser(User user);
--- src/main/java/ru/netology/netologydiplom/repository/UserRepository.java
+++ src/main/java/ru/netology/netologydiplom/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package ru.netology.netologydiplom.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.netology.netologydiplom.entity.User;
 
 import java.util.Optional;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserByUsername(String username);
 
