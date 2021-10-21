c7caa1ec68b8b5f2782b34bbbdfbe5b57a89a7d9
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-21 15:25:57 +0300

    JwtTokenProvider and SecurityConstants added

---
+++ src/main/java/ru/netology/netologydiplom/security/SecurityConstants.java
@@ -0,0 +1,10 @@
+package ru.netology.netologydiplom.security;
+
+public class SecurityConstants {
+    public static final String SIGN_UP_URLS = "/cloud/**";
+    public static final String SECRET = "SecretKeyGenJWT";
+    public static final String TOKEN_PREFIX = "Bearer ";
+    public static final String HEADER_STRING = "auth-token";
+    public static final String CONTENT_TYPE = "application/json";
+    public static final long EXPIRATION_TIME = 600_000;
+}
