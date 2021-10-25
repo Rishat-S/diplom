d8ede3312a5a15655bb0a85058cfae5559207695
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-25 09:56:31 +0300

    PasswordMatchesValidator added

---
+++ src/main/java/ru/netology/netologydiplom/validation/PasswordMatchesValidator.java
@@ -0,0 +1,21 @@
+package ru.netology.netologydiplom.validation;
+
+import ru.netology.netologydiplom.annotation.PasswordMatches;
+import ru.netology.netologydiplom.payload.request.SignupRequest;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+public class PasswordMatchesValidator implements ConstraintValidator<PasswordMatches, Object> {
+
+    @Override
+    public void initialize(PasswordMatches constraintAnnotation) {
+
+    }
+
+    @Override
+    public boolean isValid(Object obj, ConstraintValidatorContext context) {
+        SignupRequest userSignupRequest = (SignupRequest) obj;
+        return userSignupRequest.getPassword().equals(userSignupRequest.getConfirmPassword());
+    }
+}
