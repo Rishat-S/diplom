855b243f72ccc4983cd0703af5804073c8be03da
Rishat Suleymanov <spk_irbis@mail.ru>
2021-10-25 08:44:20 +0300

    JwtAuthenticationEntryPoint class added

--- src/main/java/ru/netology/netologydiplom/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/ru/netology/netologydiplom/security/JwtAuthenticationEntryPoint.java
@@ -1,2 +1,26 @@
-package ru.netology.netologydiplom.security;public class JwtAuthenticationEntryPoint {
+package ru.netology.netologydiplom.security;
+
+import com.google.gson.Gson;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+import ru.netology.netologydiplom.payload.response.InvalidLoginResponse;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e) throws IOException, ServletException {
+        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
+        String jsonLoginResponse = new Gson().toJson(loginResponse);
+        httpServletResponse.setContentType(SecurityConstants.CONTENT_TYPE);
+        httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
+        httpServletResponse.getWriter().println(jsonLoginResponse);
+    }
 }
