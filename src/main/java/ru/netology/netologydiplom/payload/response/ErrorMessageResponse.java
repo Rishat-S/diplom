8ea91124287e599cd0b9b8aef44de880f84cb188
Rishat Suleymanov <spk_irbis@mail.ru>
2021-11-11 18:46:05 +0300

    ErrorMessageResponse

---
+++ src/main/java/ru/netology/netologydiplom/payload/response/ErrorMessageResponse.java
@@ -0,0 +1,12 @@
+package ru.netology.netologydiplom.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ErrorMessageResponse {
+    private String message;
+    private int id;
+}
--- src/main/java/ru/netology/netologydiplom/security/JwtAuthenticationEntryPoint.java
+++ src/main/java/ru/netology/netologydiplom/security/JwtAuthenticationEntryPoint.java
@@ -5,7 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
-import ru.netology.netologydiplom.payload.response.InvalidLoginResponse;
+import ru.netology.netologydiplom.payload.response.ErrorMessageResponse;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -19,8 +19,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, AuthenticationException e)
             throws IOException, ServletException {
 
-        InvalidLoginResponse loginResponse = new InvalidLoginResponse();
-        String jsonLoginResponse = new Gson().toJson(loginResponse);
+        ErrorMessageResponse errorMessageResponse = ErrorMessageResponse.builder().message("Invalid Login").build();
+        String jsonLoginResponse = new Gson().toJson(errorMessageResponse);
         httpServletResponse.setContentType(SecurityConstants.CONTENT_TYPE);
         httpServletResponse.setStatus(HttpStatus.UNAUTHORIZED.value());
         httpServletResponse.getWriter().println(jsonLoginResponse);
