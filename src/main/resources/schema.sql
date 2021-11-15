d62df0bebc940eae09d7e8afbeb67c3fd64bec90
Rishat Suleymanov <spk_irbis@mail.ru>
2021-11-15 16:25:51 +0300

    edited db.changelog

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,14 +1,3 @@
-create table if not exists file
-(
-    id           bigint       not null auto_increment,
-    created_date datetime(6),
-    file_bytes   LONGBLOB,
-    name         varchar(255) not null,
-    size         bigint       not null,
-    user_id      bigint,
-    primary key (id)
-) engine = InnoDB;
-
 create table if not exists user
 (
     id         bigint       not null auto_increment,
@@ -17,20 +6,26 @@ create table if not exists user
     last_name  varchar(255) not null,
     password   varchar(3000),
     username   varchar(255),
-    primary key (id)
+    primary key (id),
+    constraint UK_ob8kqyqqgmefl0aco34akdtpe unique (email),
+    constraint UK_sb8bbouer5wak8vyiiy4pf2bx unique (username)
+) engine = InnoDB;
+
+create table if not exists file
+(
+    id           bigint       not null auto_increment,
+    created_date datetime(6),
+    file_bytes   LONGBLOB,
+    name         varchar(255) not null,
+    size         bigint       not null,
+    user_id      bigint,
+    primary key (id),
+    constraint FKinph5hu8ryc97hbs75ym9sm7t foreign key (user_id) references user (id)
 ) engine = InnoDB;
 
 create table if not exists user_role
 (
     user_id bigint not null,
-    roles   integer
+    roles   integer,
+    constraint FK859n2jvi8ivhui0rl0esws6o foreign key (user_id) references user (id)
 ) engine = InnoDB;
-
-alter table user
-    add constraint UK_ob8kqyqqgmefl0aco34akdtpe unique (email);
-alter table user
-    add constraint UK_sb8bbouer5wak8vyiiy4pf2bx unique (username);
-alter table file
-    add constraint FKinph5hu8ryc97hbs75ym9sm7t foreign key (user_id) references user (id);
-alter table user_role
-    add constraint FK859n2jvi8ivhui0rl0esws6o foreign key (user_id) references user (id);
