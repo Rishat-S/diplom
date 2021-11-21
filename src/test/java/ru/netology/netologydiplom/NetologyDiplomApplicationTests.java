f82e93783af57862c8a201693991d4ac2c1f87c5
Rishat Suleymanov <spk_irbis@mail.ru>
2021-11-21 20:43:40 +0300

    test liquibase edited

--- src/test/java/ru/netology/netologydiplom/NetologyDiplomApplicationTests.java
+++ src/test/java/ru/netology/netologydiplom/NetologyDiplomApplicationTests.java
@@ -1,9 +1,20 @@
 package ru.netology.netologydiplom;
 
-import org.junit.Test;
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.transaction.annotation.Transactional;
+import ru.netology.netologydiplom.entity.User;
+import ru.netology.netologydiplom.exceptions.FileNotFoundException;
+import ru.netology.netologydiplom.repository.FileRepository;
+import ru.netology.netologydiplom.repository.UserRepository;
 import ru.netology.netologydiplom.service.FileService;
+
+import java.security.Principal;
 
 @SpringBootTest
 class NetologyDiplomApplicationTests {
@@ -11,8 +22,26 @@ class NetologyDiplomApplicationTests {
     @Autowired
     private FileService fileService;
 
+    @Autowired
+    private FileRepository fileRepository;
+
+    @Autowired
+    private UserRepository userRepository;
+
     @Test
-    public void createUserTest() {
+    @Transactional
+    public void test() {
+        Principal mockPrincipal = Mockito.mock(Principal.class);
+        Mockito.when(mockPrincipal.getName()).thenReturn("user");
+
+        User user = userRepository.findUserByUsername(mockPrincipal.getName())
+                .orElseThrow(() -> new UsernameNotFoundException("Username not found"));
+
+        var file = fileRepository.findFileByNameAndUser("name", user)
+                .orElseThrow(() -> new FileNotFoundException("File cannot be found"));
+
+        Assertions.assertNotNull(file);
+
     }
 
 
