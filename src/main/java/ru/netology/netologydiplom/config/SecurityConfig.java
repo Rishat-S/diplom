ce02a9cac743c1637f08dc2c82c50abcb1afe30a
Rishat Suleymanov <spk_irbis@mail.ru>
2021-11-22 10:18:21 +0300

    SecurityConfig edited

--- src/main/java/ru/netology/netologydiplom/config/SecurityConfig.java
+++ src/main/java/ru/netology/netologydiplom/config/SecurityConfig.java
@@ -35,7 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors(withDefaults()).csrf().disable()
+        http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint)
                 .and()
                 .sessionManagement()
